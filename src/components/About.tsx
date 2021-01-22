9c7c5eaed2d553b4a23d396eaf735f293957db06
Sergio Villanueva <sergionicolasvillanueva@gmail.com>
2021-01-22 12:43:33 -0300

    Update About.tsx

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,8 +1,19 @@
 import React from 'react';
-import './Components.css';
 
-const About: React.FC = () => (<div>
-    <p>About!</p>
-</div>);
+const About: React.FC = () => {
+
+    const style: React.CSSProperties = {
+        margin: "0",
+        position: "absolute",
+        left: "50%",
+        transform: "translateX(-50%)"
+    };
+
+    return (
+        <div style={style}>
+            <p>Sergio Villanueva!</p>
+        </div>
+    );
+};
 
 export default About;
