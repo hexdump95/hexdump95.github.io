9ef6b16aba41d88c7ffa1274896fd493706a71d2
Sergio Villanueva <sergionicolasvillanueva@gmail.com>
2021-01-22 12:43:10 -0300

    Update Contact.tsx

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
-import './Components.css';
 
-const Contact: React.FC = () => <div>
-    <p>Contact!</p>
-</div>;
+const Contact: React.FC = () => {
+    return (<div>
+        <p><b>Email:</b> <a href="mailto:sergionicolasvillanueva@gmail.com">sergionicolasvillanueva@gmail.com</a></p>
+        <p><b>GitHub:</b> <a href="http://github.com/hexdump95">hexdump95</a></p>
+        <p><b>Linkedin:</b> <a href="https://www.linkedin.com/in/sergiovillanueva95/?locale=en_US">Sergio Villanueva</a></p>
+    </div>);
+};
 
 export default Contact;
